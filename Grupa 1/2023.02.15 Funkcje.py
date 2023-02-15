1cb2ecbe11c91a402c6f65f256c734bd1d18652f
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-02-16 00:05:44 +0100

    Update

--- Grupa 1/2023.02.15 Funkcje.py
+++ Grupa 1/2023.02.15 Funkcje.py
@@ -1,20 +1,23 @@
+#przykład funkcji, która zwraca sumę argumentów
+
 def dodaj(a,b):
     return a+b
 
+#przykład funkcji, która zwraca iloraz dwóch argumentów lub "NaN" jeśli dzielenie przez 0
 def podziel(a,b):
     if b == 0:
         #print("nie można dzielić przez zero!")
-        return "Inf"
+        return "NaN"
     else:
         return a/b
     
-    
+#przykład funkcji, która wyświetla sekwencję liczb od a do b co skok c: 
 def sekwencja(a,b,c):
     i = a
     while i<b:
         print(i,end=",")
         i += c
-
+#funkcja sprawdzająca czy podana liczba jest pierwsza
 def czyPierwsza(a):
     i=2
     while i <= a**0.5:
@@ -23,7 +26,7 @@ def czyPierwsza(a):
         i+=1
     return True
 
-
+#funkcja, która generuje liczby pierwsze z określonego zakresu
 def pierwsze(a,b):
     i = a
     while i<b:
@@ -31,4 +34,5 @@ def pierwsze(a,b):
             print(i,end=",")
         i += 1
 
+#wywołanie funkcji
 pierwsze(2,100000)
