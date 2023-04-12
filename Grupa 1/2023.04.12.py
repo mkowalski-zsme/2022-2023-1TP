3f431e640cf69280d9eb027ad48b788b206858ff
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-12 13:58:32 +0200

    update

---
+++ Grupa 1/2023.04.12.py
@@ -0,0 +1,13 @@
+#importowanie bibliotek
+
+import random as rnd
+
+#import lib
+# from lib import suma,iloczyn
+# from lib import *
+import lib as lb
+
+
+print(lb.suma(3,4))
+
+
---
+++ Grupa 1/lib.py
@@ -0,0 +1,20 @@
+def suma(a,b):
+    return a+b
+
+
+def iloczyn(a,b):
+    return a*b
+
+
+#print(suma(4,6))
+
+
+def suma2(*arg):
+    pass
+
+
+def suma3(**arg):
+    pass
+
+
+suma3(ania=3,zosia = 4)
