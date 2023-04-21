c46cb45fb67a31e3903b8fff84ffa88be95b1b5f
Marcin Kowalski <m.kowalski@zsme.tarnow.pl>
2023-04-21 09:36:57 +0200

    Update

---
+++ Grupa 2/2023.04.21 Lekcja.py
@@ -0,0 +1,27 @@
+# import random
+# print(random.randint(4,112))
+
+
+# import random as rnd
+# print(rnd.randint(4,112))
+
+# from random import randint, random
+
+# print(randint(3,123))
+# print(random())
+
+
+# from random import *
+
+# print(random())
+# print(randint(3,123))
+
+# from math import *
+# from numpy import *
+
+# print(sqrt(4))
+
+import biblioteka as lib
+from biblioteka import Suma as sm
+
+print(sm(3,4))
